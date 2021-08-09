12b3573cbf02fe0eac56d58cdc9276b388ac1fb3
toycrane <alwaysfun2183@gmail.com>
2021-08-09 17:37:06 +0900

    feature: 버튼 클릭 시, ordering하는 함수 구현

--- main.js
+++ main.js
@@ -17,3 +17,30 @@ fetch(
 		});
 		tableBody.innerHTML += trs;
 	});
+
+const handleOrdering = () => {
+	fetch(
+		"https://raw.githubusercontent.com/weffwd/take-home-assessment/main/data.json"
+	)
+		.then((response) => response.json())
+		.then((transactions) => {
+			const sortedTransactions = transactions.sort((a, b) =>
+				a.price > b.price ? 1 : -1
+			);
+			let trs = "";
+			tableBody.innerHTML = "";
+			sortedTransactions.map((transaction, index) => {
+				let className = "";
+				if (index % 2 === 0) {
+					className = "stock-table__row--even";
+				} else {
+					className = "stock-table__row--odd";
+				}
+				trs += `<tr class=${className}><td>${transaction.stock.name}</td><td>${transaction.price}</td></tr>`;
+			});
+			tableBody.innerHTML += trs;
+		});
+};
+
+const descOrderingButton = document.getElementById("ordering-button__desc");
+descOrderingButton.addEventListener("click", handleOrdering);
