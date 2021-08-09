5cc53f2833ea74b3883aafc0459fc8f0a97ba8aa
toycrane <alwaysfun2183@gmail.com>
2021-08-09 17:44:25 +0900

    feature: createTableRowElement 모듈화

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
-let tableBody = document.getElementById("stock-table__body");
+const createTableRowElement = (transactions) => {
+	let trs = "";
+	transactions.map((transaction, index) => {
+		let className = "";
+		if (index % 2 === 0) {
+			className = "stock-table__row--even";
+		} else {
+			className = "stock-table__row--odd";
+		}
+		trs += `<tr class=${className}><td>${transaction.stock.name}</td><td>${transaction.price}</td></tr>`;
+	});
+	return trs;
+};
 
 fetch(
 	"https://raw.githubusercontent.com/weffwd/take-home-assessment/main/data.json"
 )
 	.then((response) => response.json())
 	.then((transactions) => {
-		let trs = "";
-		transactions.map((transaction, index) => {
-			let className = "";
-			if (index % 2 === 0) {
-				className = "stock-table__row--even";
-			} else {
-				className = "stock-table__row--odd";
-			}
-			trs += `<tr class=${className}><td>${transaction.stock.name}</td><td>${transaction.price}</td></tr>`;
-		});
+		let tableBody = document.getElementById("stock-table__body");
+		tableBody.innerHTML = "";
+		let trs = createTableRowElement(transactions);
 		tableBody.innerHTML += trs;
 	});
 
@@ -24,20 +29,12 @@ const handleOrdering = () => {
 	)
 		.then((response) => response.json())
 		.then((transactions) => {
+			let tableBody = document.getElementById("stock-table__body");
 			const sortedTransactions = transactions.sort((a, b) =>
 				a.price > b.price ? -1 : 1
 			);
-			let trs = "";
 			tableBody.innerHTML = "";
-			sortedTransactions.map((transaction, index) => {
-				let className = "";
-				if (index % 2 === 0) {
-					className = "stock-table__row--even";
-				} else {
-					className = "stock-table__row--odd";
-				}
-				trs += `<tr class=${className}><td>${transaction.stock.name}</td><td>${transaction.price}</td></tr>`;
-			});
+			let trs = createTableRowElement(sortedTransactions);
 			tableBody.innerHTML += trs;
 		});
 };
